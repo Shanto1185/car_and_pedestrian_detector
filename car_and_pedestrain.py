a08505ae3a82e7b38287d956e6cd29adbb446857
najumul hasan <you@example.com>
2023-07-22 01:13:06 +0600

    modified

--- car_and_pedestrain.py
+++ car_and_pedestrain.py
@@ -49,35 +49,3 @@ while True:
 
     # Don't autoclose (wait here in the code and listen for a key)
     cv2.waitKey(1)
-
-
-"""
-# create opencv image
-img = cv2.imread(img_file)
-
-# Convert to grayscale (needed fo haar cascade)
-black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# create car classifier
-car_tracker = cv2.CascadeClassifier(classifier_file)
-
-# detect cars
-cars = car_tracker.detectMultiScale(black_n_white)
-
-print(cars)
-
-# Draw rectangles around the cars
-for (x, y, w, h) in cars:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-
-
-# Display the image with fhe faces spotted
-cv2.imshow('shanto', img)
-
-# Don't autoclose (wait here in the code and listen for a key press)
-cv2.waitKey()
-
-
-"""
-
-print("Code Completed")
