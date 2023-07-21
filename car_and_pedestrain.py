c05042d83e14d3ddf3ba88fd38728a97a3fc1cf7
najumul hasan <you@example.com>
2023-07-22 00:54:49 +0600

    first commit

---
+++ car_and_pedestrain.py
@@ -0,0 +1,83 @@
+import cv2
+
+
+# our image
+img_file = 'car2.jpg'
+# video = cv2.VideoCapture('tesla dash cam.mp4')
+video = cv2.VideoCapture('motor_das_cam.mp4')
+
+
+# pre trained car and pedestrian classifiers
+car_tracker_file = 'car_detector.xml'
+pedestrian_tracker_file = 'haarcascase_fullbody.xml'
+
+
+# create car and pedetrian classifiers
+car_tracker = cv2.CascadeClassifier(car_tracker_file)
+pedestrian_tracker = cv2.CascadeClassifier(pedestrian_tracker_file)
+
+
+# Run forever untill car stops
+while True:
+
+    # Read the current frame
+    (read_successful, frame) = video.read()
+
+    # Safe coding
+    if read_successful:
+        # Must convert to grayscale
+        grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    else:
+        break
+
+    # detect cars and pedestrians
+    cars = car_tracker.detectMultiScale(grayscaled_frame)
+    pedestrians = pedestrian_tracker.detectMultiScale(grayscaled_frame)
+
+    # print(cars)
+
+    # Draw rectangles around the cars
+    for (x, y, w, h) in cars:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+    # Draw rectangles around the pedestrian
+    for (x, y, w, h) in pedestrians:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
+
+    # Display the image with fhe faces spotted
+    cv2.imshow('shanto', frame)
+
+    # Don't autoclose (wait here in the code and listen for a key)
+    cv2.waitKey(1)
+
+
+"""
+# create opencv image
+img = cv2.imread(img_file)
+
+# Convert to grayscale (needed fo haar cascade)
+black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# create car classifier
+car_tracker = cv2.CascadeClassifier(classifier_file)
+
+# detect cars
+cars = car_tracker.detectMultiScale(black_n_white)
+
+print(cars)
+
+# Draw rectangles around the cars
+for (x, y, w, h) in cars:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+
+# Display the image with fhe faces spotted
+cv2.imshow('shanto', img)
+
+# Don't autoclose (wait here in the code and listen for a key press)
+cv2.waitKey()
+
+
+"""
+
+print("Code Completed")
